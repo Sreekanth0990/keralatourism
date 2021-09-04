4e23752486319abcfd335b703d84a210137b3bbe
sreekanthambady <41562119+sreekanthambady@users.noreply.github.com>
2021-09-04 16:13:58 +0530

    validation

--- js/script.js
+++ js/script.js
@@ -42,177 +42,149 @@ const setBgOverlay = () => {
     }
 }
 
-$(document).ready(function () {
-    const autoplayBg = true;	// set Auto Play for Background Images
-    initBg(autoplayBg);    
-    setBgOverlay();
 
-    const bgControl = $('.tm-bg-control');            
-    bgControl.click(function() {
-        bgControl.removeClass('active');
-        $(this).addClass('active');
-        const id = $(this).data('id');                
-        setBg(id);
-    });
 
-    $(window).on("backstretch.after", function (e, instance, index) {        
-        const bgControl = $('.tm-bg-control');
-        bgControl.removeClass('active');
-        const current = $(".tm-bg-controls-wrapper").find(`[data-id=${index}]`);        
-        current.addClass('active');
-    });
+const form=document.getElementById('form');
+const formlog=document.getElementById('formlog');
+const username=document.getElementById('username');
+const email=document.getElementById('email');
+const password=document.getElementById('password');
+const password2=document.getElementById('password2');
+const mobile=document.getElementById('mobile');
 
-    $(window).resize(function() {
-        setBgOverlay();
-    });
+
+
+
+//Show input error message
+
+function showError(input,message){
+    const formControl=input.parentElement;
+    formControl.className='form-control error';
+    const small=formControl.querySelector('small');
+    small.innerText=message;
+}
+
+function showSuccess(input){
+    const formControl=input.parentElement;
+    formControl.className='form-control success';
+    
+}
+
+
+
+//Email
+
+function isValidEmail(email)
+{
+    const re= /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+//Password
+
+function isValidPassword(password)
+{
+    const psr= /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return psr.test(String(password));
+}
+
+//passwordmatch
+
+function isValidPasswordmatch()
+{
+    if(password.value!=password2.value)
+    {
+                return false;
+    }
+    return true;
+}
+
+//phone number
+
+function isValidmobile(mobile)
+{
+    const mob= /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+    return mob.test(String(mobile));
+}
+
+
+
+
+
+// function loginval(){
+   
+
+// if(email.value===''){
+//         showError(email,'Please enter Registered Email');
+//     }else if(!isValidEmail(email.value)){
+//         showError(email,'Please enter Valid Email');
+//     }
+
+//     if(password.value===''){
+//         showError(password,'Please enter Password');
+//     }else if (!isValidPassword(password.value)){
+//         showError(password,"Password is not valid .");       
+
+//     }
+// }
+
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+
+    if(username.value===''){
+        showError(username,'Username is required');
+    }
+    else{
+        showSuccess(username);
+    }
+    if(email.value===''){
+        showError(email,'Email is required');
+    }else if(!isValidEmail(email.value)){
+        showError(email,'Email is not valid');
+    }
+    else{
+        showSuccess(email);
+    }
+
+    if(mobile.value===''){
+        showError(mobile,'Phone number is required');
+    }else if(!isValidmobile(mobile.value)){
+        showError(mobile,'Phone number is not valid');
+    }
+    else{
+        showSuccess(mobile);
+    }
+
+    if(password.value===''){
+        showError(password,'Password is required');
+    }else if (!isValidPassword(password.value)){
+        showError(password,"Password is not valid .");
+        // window.alert("Password should be of minimum 8 characters, at least one uppercase, and one lower case, must contain at least one number");
+
+    }
+    else{
+        showSuccess(password);
+    }
+    if(password2.value===''){
+        showError(password2,'confirm password is required');
+    }else if (isValidPasswordmatch()==false){
+        showError(password2,"Passwords Doesnt Match, Please Re-enter :");
+    }
+    else{
+        showSuccess(password2);
+    }
+    Regsuccess();
+    
 });
 
-var mail = document.getElementById("email");
-var pswd = document.getElementById("password");
-function lvalidate()
-{
-    var res = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    var psr = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-    
+function Regsuccess(){
 
-    if(res.test(mail.value)!="1")
+    if(isValidEmail(email.value)&&isValidmobile(mobile.value)&&isValidPassword(password.value)&&isValidPasswordmatch()==true)
     {
-        window.alert("Email Is Invalid");
-        return false;
+     alert("You Registration was successful");
+
     }
-    else if(psr.test(pswd.value)!="1")
-    {
-        window.alert("Password Is Invalid, try again");
-        window.alert("Password should be of minimum 8 characters, at least one uppercase, and one lower case, must contain at least one number");
 
-        return false;
-    }
-    else
-    {
-        window.alert("validation Success");
-        return true;
-    }
-    
-};
+}
 
 
-var smail = document.getElementById("semail");
-var spswd = document.getElementById("password1");
-var spswd2 = document.getElementById("password2");
-var phone = document.getElementById("phone");
-
-function svalidate()
-{
-    var res = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    var psr = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-    var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-    
-
-    if(res.test(smail.value)!="1")
-    {
-        
-        window.alert("Email Is Invalid");
-        return false;
-    }
-    else if(phoneno.test(phone.value)!="1")
-    {
-        window.alert("Please enter a valid phone number :");
-        return false;
-    }
-    else if(psr.test(spswd.value)!="1")
-    {
-        window.alert("Password Is Invalid");
-        window.alert("Password should be of minimum 8 characters, at least one uppercase, and one lower case, must contain at least one number");
-        window.alert("try again");
-        return false;
-    }
-    else  if(spswd.value!=spswd2.value)
-    {
-        window.alert("Passwords Doesnt Match, Please Re-enter :");
-        return false;
-    }
-    else
-    {
-        window.alert("validation Success");
-        return true;
-    }
-    
-};
-
-
-$(document).ready(function () {
-
-    $('#password1').on('keyup', function () {
-
-        let textElement = $(this).val()
-        let strength = 0
-
-        $('#typepass').find('h4').html(`Your Password: ${textElement}`)
-
-        if (textElement.length > 0) {
-            let sizeElements = textElement.length
-
-            if (sizeElements > 10) {
-
-                strength += 30
-
-            } else {
-                let calcMath = (sizeElements * 2)
-
-                strength += calcMath
-
-            }
-
-        }
-
-        let lowerCase = new RegExp(/[a-z]/)
-        if (lowerCase.test(textElement)) {
-            strength += 16
-        }
-
-        let upperCase = new RegExp(/[A-Z]/)
-        if (upperCase.test(textElement)) {
-            strength += 18
-        }
-
-        let regularNumber = new RegExp(/[0-9]/i)
-        if (regularNumber.test(textElement)) {
-            strength += 16
-        }
-
-        let specialChars = new RegExp(/[^a-z0-9]/i)
-        if (specialChars.test(textElement)) {
-            strength += 20
-        }
-        
-        //Function to produce result
-        let renderResult = (strengthData, color) => {
-            return $('#strengthResult').html(
-                `
-                          
-            <div class="progress" style="height: 40px;">
-                <div class="progress-bar bg-${color}" role="progressbar" style="width: ${strengthData}%" aria-valuenow="${strengthData}" aria-valuemin="0" aria-valuemax="100"><strong style="font-size: 15px">${strength}%</strong></div>
-            </div>`
-            )
-        }
-
-        if (strength < 21) {
-            renderResult(strength, 'danger')//red very weak password
-        } else
-            if (strength > 20 && strength < 41) {
-                renderResult(strength, 'warning')//orange weak password
-            } else
-                if (strength > 40 && strength < 61) {
-                    renderResult(strength, 'secondary')//medium password
-                } else
-                    if (strength > 60 && strength < 81) {
-                        renderResult(strength, 'info')// strong password
-                    } else {
-                        renderResult(strength, 'success')//very strong password
-                    }
-
-        
-
-    });
-
-});
